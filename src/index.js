f99e7f9390c04872fd4fe8d450ace2e2dd8f1090
viruzvz <viruzvz@gmail.com>
2020-12-02 13:25:33 -0300

    fontawesome

--- src/index.js
+++ src/index.js
@@ -6,10 +6,3 @@ import 'popper.js'
 import 'bootstrap'
 
 window.$ = window.jQuery = $
-
-const onDOMContentLoaded = function () {
-  const $window = $(window)
-  const $doc = $(document)
-}
-
-$(onDOMContentLoaded)
